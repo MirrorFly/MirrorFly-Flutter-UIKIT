5577d63f6ce4ed30b0a656dd285cd1d44acff87a
Mani Vendhan M <manivendhan.m@contus.in>
2023-08-19 02:28:45 +0530

    #0.0.10 Release Changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,10 +6,10 @@ import 'package:mirrorfly_uikit_plugin/mirrorfly_uikit.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MirrorflyUikit.instance.initUIKIT(
-      baseUrl: 'https://api-uikit-qa.contus.us/api/v1/',
-      licenseKey: 'ckIjaccWBoMNvxdbql8LJ2dmKqT5bp',
-      googleMapKey: 'AIzaSyBaKkrQnLT4nacpKblIE5d4QK6GpaX5luQ',
-      iOSContainerID: 'group.com.mirrorfly.flutter');
+      baseUrl: 'YOUR_BASE_URL',
+      licenseKey: 'Your_Mirrorfly_Licence_Key',
+      googleMapKey: 'Your_Google_Map_Key_for_location_messages',
+      iOSContainerID: 'Your_iOS_app_Container_id');
   runApp(const MyApp());
 }
 
